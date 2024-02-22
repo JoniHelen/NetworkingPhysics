95d528e4eaba82b88044ab1c8ca2eed0a6a0cdf0
Joni Helén <91052459+JoniHelen@users.noreply.github.com>
2024-02-22 13:30:29 +0200

    Initial commit

---
+++ NetworkingPhysics.cpp
@@ -0,0 +1,12 @@
+﻿// NetworkingPhysics.cpp : Defines the entry point for the application.
+//
+
+#include "NetworkingPhysics.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
