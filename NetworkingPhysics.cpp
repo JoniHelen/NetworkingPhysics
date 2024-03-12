a67e753dc7d4826ef2e16cd1c95698e9fdbf56a4
JoniHelen <djohe015@edu.xamk.fi>
2024-03-12 09:16:59 +0200

    asio install

--- NetworkingPhysics.cpp
+++ NetworkingPhysics.cpp
@@ -4,228 +4,272 @@
 
 using namespace std;
 
-static const char* vertex_shader_text =
-"#version 330\n"
-"layout(location = 0) in vec2 vPos;\n"
-"layout(location = 1) in vec3 vCol;\n"
-"layout(location = 2) in mat4 ModelMatrix;\n"
-"uniform mat4 ViewMatrix;\n"
-"uniform mat4 ProjMatrix;\n"
-"out vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = ProjMatrix * ViewMatrix * ModelMatrix * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
+namespace {
+	constexpr const char* vertex_shader_text =
+		"#version 330\n"
+		"in vec2 vPos;\n"
+		"in vec3 vCol;\n"
+		"in mat4 ModelMatrix;\n"
+		"uniform mat4 ViewMatrix;\n"
+		"uniform mat4 ProjMatrix;\n"
+		"out vec3 color;\n"
+		"void main()\n"
+		"{\n"
+		"    gl_Position = ProjMatrix * ViewMatrix * ModelMatrix * vec4(vPos, 0.0, 1.0);\n"
+		"    color = vCol;\n"
+		"}\n";
 
-static const char* fragment_shader_text =
-"#version 330\n"
-"in vec3 color;\n"
-"out vec4 fragColor;\n"
-"void main()\n"
-"{\n"
-"    fragColor = vec4(color, 1.0);\n"
-"}\n";
+	constexpr const char* fragment_shader_text =
+		"#version 330\n"
+		"in vec3 color;\n"
+		"out vec4 fragColor;\n"
+		"void main()\n"
+		"{\n"
+		"    fragColor = vec4(color, 1.0);\n"
+		"}\n";
 
-static const struct Vertex
-{
-	float x, y;
-	float r, g, b;
-} vertices[3] =
-{
-	{ -0.04330127f, -0.025f, 1.f, 1.f, 0.f },
-	{  0.04330127f, -0.025f, 0.f, 1.f, 1.f },
-	{   0.0f,  0.05f, 1.f, 0.f, 1.f }
-};
-
-constexpr auto COUNT_TRIANGLES = 50;
-
-static const uint32 indices[3] = { 0, 1, 2 };
-static b2Body* walls[4];
-static b2Body* triangles[COUNT_TRIANGLES];
-static mat4x4 triangleTransforms[COUNT_TRIANGLES];
-
-void create_world_bounds(const unique_ptr<b2World>& world) {
-	b2BodyDef bodyDef;
-
-	// wall 1
-	bodyDef.position.Set(0, -1.5);
-	walls[0] = world->CreateBody(&bodyDef);
-
-	// wall 2
-	bodyDef.position.Set(0, 1.5);
-	walls[1] = world->CreateBody(&bodyDef);
-
-	// wall 3
-	bodyDef.position.Set(1.5, 0);
-	walls[2] = world->CreateBody(&bodyDef);
-
-	// wall 4
-	bodyDef.position.Set(-1.5, 0);
-	walls[3] = world->CreateBody(&bodyDef);
-
-	b2PolygonShape polygonShapeV;
-	polygonShapeV.SetAsBox(0.5f, 20);
-
-	b2PolygonShape polygonShapeH;
-	polygonShapeH.SetAsBox(20, 0.5f);
-
-	walls[0]->CreateFixture(&polygonShapeH, 0.0f);
-	walls[1]->CreateFixture(&polygonShapeH, 0.0f);
-	walls[2]->CreateFixture(&polygonShapeV, 0.0f);
-	walls[3]->CreateFixture(&polygonShapeV, 0.0f);
-}
-
-void create_physics_triangles(const unique_ptr<b2World>& world) {
-	b2BodyDef dynamicBodyDef;
-	dynamicBodyDef.type = b2_dynamicBody;
-	dynamicBodyDef.angle = 0;
-
-	const b2Vec2 triangle[3] = {
-		{ -0.04330127f, -0.025f },
-		{  0.04330127f, -0.025f },
-		{   0.0f,  0.05f }
+	struct Vertex {
+		vec2 pos;
+		vec3 col;
 	};
 
-	b2PolygonShape dynamicShape;
-	dynamicShape.Set(triangle, 3);
+	constexpr Vertex vertices[3] = {
+		{ -0.4330127f, -0.25f, 1.0f, 1.0f, 0.0f },
+		{  0.4330127f, -0.25f, 0.0f, 1.0f, 1.0f },
+		{   0.0f,  0.5f, 1.0f, 0.0f, 1.0f }
+	};
 
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicShape;
-	fixtureDef.density = 1.0f;
-	fixtureDef.friction = 0.3f;
-	fixtureDef.restitution = 1;
+	constexpr int COUNT_TRIANGLES = 50;
 
-	for (int i = 0; i < COUNT_TRIANGLES; i++) {
-		dynamicBodyDef.linearVelocity.Set(static_cast<float>((i % 10 - 4) * 0.5), static_cast<float>(i / 10 * 0.5));
-		dynamicBodyDef.position.Set(static_cast<float>((i % 10 - 4) * 0.1), static_cast<float>(i / 10 * 0.1));
-		
-		triangles[i] = world->CreateBody(&dynamicBodyDef);
-		triangles[i]->CreateFixture(&fixtureDef);
+	constexpr uint32 indices[3] = { 0u, 1u, 2u };
+	b2Body* walls[4u];
+	b2Body* triangles[COUNT_TRIANGLES];
+	mat4x4 triangleTransforms[COUNT_TRIANGLES];
+
+	/**
+	 * \brief Creates walls around the scene with physics objects.
+	 * \param world The world in which the objects are instantiated
+	 */
+	void create_world_bounds(const unique_ptr<b2World>& world) {
+		// Blank physics body template
+		b2BodyDef bodyDef;
+
+		// wall 1
+		bodyDef.position.Set(0, -7);
+		walls[0] = world->CreateBody(&bodyDef);
+
+		// wall 2
+		bodyDef.position.Set(0, 7);
+		walls[1] = world->CreateBody(&bodyDef);
+
+		// wall 3
+		bodyDef.position.Set(7, 0);
+		walls[2] = world->CreateBody(&bodyDef);
+
+		// wall 4
+		bodyDef.position.Set(-7, 0);
+		walls[3] = world->CreateBody(&bodyDef);
+
+		// Vertical and horizontal wall shapes
+		b2PolygonShape polygonShapeV;
+		polygonShapeV.SetAsBox(0.5f, 20);
+
+		b2PolygonShape polygonShapeH;
+		polygonShapeH.SetAsBox(20, 0.5f);
+
+		// Attach shapes to bodies and make them have infinite mass
+		walls[0]->CreateFixture(&polygonShapeH, 0.0f);
+		walls[1]->CreateFixture(&polygonShapeH, 0.0f);
+		walls[2]->CreateFixture(&polygonShapeV, 0.0f);
+		walls[3]->CreateFixture(&polygonShapeV, 0.0f);
 	}
-}
 
-void reset_simulation() {
-	for (int i = 0; i < COUNT_TRIANGLES; i++) {
-		triangles[i]->SetLinearVelocity(b2Vec2(static_cast<float>((i % 10 - 4) * 0.5), static_cast<float>(i / 10 * 0.5)));
-		triangles[i]->SetTransform(b2Vec2(static_cast<float>((i % 10 - 4) * 0.1), static_cast<float>(i / 10 * 0.1)), 0);
+	/**
+	 * \brief Creates triangular physics bodies into the world.
+	 * \param world The world in which the triangles are instantiated
+	 */
+	void create_physics_triangles(const unique_ptr<b2World>& world) {
+
+		// Convert vertices into b2Vec2 array
+		const b2Vec2 triangle[3] = {
+			{ vertices[0].pos[0], vertices[0].pos[1] },
+			{ vertices[1].pos[0], vertices[1].pos[1] },
+			{ vertices[2].pos[0], vertices[2].pos[1] }
+		};
+
+		//  Define fixture
+		b2PolygonShape triangleShape;
+		triangleShape.Set(triangle, 3);
+
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &triangleShape;
+		fixtureDef.density = 6.0f;
+		fixtureDef.friction = 0.3f;
+		fixtureDef.restitution = 0.0f;
+
+		// Define physics body
+		b2BodyDef dynamicBodyDef;
+		dynamicBodyDef.type = b2_dynamicBody;
+
+		// Create triangle objects
+		for (int i = 0; i < COUNT_TRIANGLES; i++) {
+			dynamicBodyDef.linearVelocity.Set(static_cast<float>((i % 10 - 4)), static_cast<float>(i / 10));
+			dynamicBodyDef.position.Set(static_cast<float>((i % 10 - 4)), static_cast<float>(i / 10));
+
+			triangles[i] = world->CreateBody(&dynamicBodyDef);
+			triangles[i]->CreateFixture(&fixtureDef);
+		}
 	}
-}
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-	if (key == GLFW_KEY_R && action == GLFW_PRESS) {
-		reset_simulation();
+	/**
+	 * \brief Resets the simulation to its starting state.
+	 */
+	void reset_simulation() {
+		for (int i = 0; i < COUNT_TRIANGLES; i++) {
+			triangles[i]->SetLinearVelocity(b2Vec2(static_cast<float>((i % 10 - 4)), static_cast<float>(i / 10)));
+			triangles[i]->SetTransform(b2Vec2(static_cast<float>((i % 10 - 4)), static_cast<float>(i / 10)), 0);
+			triangles[i]->SetAngularVelocity(0);
+		}
 	}
-}
 
-void error_callback(int error, const char* description)
-{
-	cerr << "Error: " << description << endl;
+	void key_callback(GLFWwindow* const window, const int key, const int scancode, const int action, const int mods)
+	{
+		if (key == GLFW_KEY_R && action == GLFW_PRESS) {
+			reset_simulation();
+		}
+	}
+
+	void error_callback(const int error, const char* const description)
+	{
+		cerr << "Error: " << description << endl;
+	}
+
+	GLFWwindow* init_window()
+	{
+		if (!glfwInit()) exit(-1);
+		glfwSetErrorCallback(error_callback);
+
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+		GLFWwindow* window = glfwCreateWindow(1280, 720, "Synced Physics", nullptr, nullptr);
+
+		if (!window) {
+			error_callback(-1, "Window creation failed.");
+			glfwTerminate();
+			exit(-1);
+		}
+
+		glfwMakeContextCurrent(window);
+		gladLoadGL(glfwGetProcAddress);
+		glfwSwapInterval(1);
+
+		glfwSetKeyCallback(window, key_callback);
+
+		return window;
+	}
+
+	void init_ImGui(GLFWwindow* const window)
+	{
+		ImGui::CreateContext();
+
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init("#version 330");
+
+		ImGui::StyleColorsDark();
+	}
+
+	GLuint generate_shader_program()
+	{
+		const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertex_shader, 1, &vertex_shader_text, nullptr);
+		glCompileShader(vertex_shader);
+
+		const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragment_shader, 1, &fragment_shader_text, nullptr);
+		glCompileShader(fragment_shader);
+
+		const GLuint program = glCreateProgram();
+		glAttachShader(program, vertex_shader);
+		glAttachShader(program, fragment_shader);
+		glLinkProgram(program);
+
+		return program;
+	}
+
+	void generate_buffers(const GLuint& program, GLuint& vertex_buffer, GLuint& transform_buffer, GLuint& index_buffer, GLuint& vertex_array)
+	{
+		const GLint model_location = glGetAttribLocation(program, "ModelMatrix");
+		const GLint vpos_location = glGetAttribLocation(program, "vPos");
+		const GLint vcol_location = glGetAttribLocation(program, "vCol");
+
+		glGenVertexArrays(1, &vertex_array);
+		glBindVertexArray(vertex_array);
+
+		glGenBuffers(1, &vertex_buffer);
+		glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(vpos_location);
+		glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+			sizeof(vertices[0]), static_cast<void*>(nullptr));
+
+		glEnableVertexAttribArray(vcol_location);
+		glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+			sizeof(vertices[0]), reinterpret_cast<void*>(sizeof(float) * 2));
+
+		glGenBuffers(1, &transform_buffer);
+		glBindBuffer(GL_ARRAY_BUFFER, transform_buffer);
+		glBufferData(GL_ARRAY_BUFFER, COUNT_TRIANGLES * sizeof(mat4x4), triangleTransforms, GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(model_location);
+		glVertexAttribPointer(model_location, 4, GL_FLOAT, GL_FALSE,
+			sizeof(mat4x4), static_cast<void*>(nullptr));
+
+		glEnableVertexAttribArray(model_location + 1);
+		glVertexAttribPointer(model_location + 1, 4, GL_FLOAT, GL_FALSE,
+			sizeof(mat4x4), reinterpret_cast<void*>(1 * sizeof(vec4)));
+
+		glEnableVertexAttribArray(model_location + 2);
+		glVertexAttribPointer(model_location + 2, 4, GL_FLOAT, GL_FALSE,
+			sizeof(mat4x4), reinterpret_cast<void*>(2 * sizeof(vec4)));
+
+		glEnableVertexAttribArray(model_location + 3);
+		glVertexAttribPointer(model_location + 3, 4, GL_FLOAT, GL_FALSE,
+			sizeof(mat4x4), reinterpret_cast<void*>(3 * sizeof(vec4)));
+
+		glVertexAttribDivisor(model_location, 1);
+		glVertexAttribDivisor(model_location + 1, 1);
+		glVertexAttribDivisor(model_location + 2, 1);
+		glVertexAttribDivisor(model_location + 3, 1);
+
+		glGenBuffers(1, &index_buffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	}
 }
 
 int main()
 {
-	if (!glfwInit()) return -1;
-	glfwSetErrorCallback(error_callback);
+	GLFWwindow* window = init_window();
+	init_ImGui(window);
+	const ImGuiIO& io = ImGui::GetIO(); (void)io;
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	const GLuint program = generate_shader_program();
 
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "Synced Physics", nullptr, nullptr);
+	const GLint v_location = glGetUniformLocation(program, "ViewMatrix");
+	const GLint p_location = glGetUniformLocation(program, "ProjMatrix");
 
-	if (!window) {
-		error_callback(-1, "Window creation failed.");
-		glfwTerminate();
-		return -1;
-	}
+	GLuint vertex_buffer, transform_buffer, index_buffer, vertex_array;
+	generate_buffers(program, vertex_buffer, transform_buffer, index_buffer, vertex_array);
 
-	glfwMakeContextCurrent(window);
-	gladLoadGL(glfwGetProcAddress);
-	glfwSwapInterval(1);
-
-	glfwSetKeyCallback(window, key_callback);
-
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 330");
-
-	ImGui::StyleColorsDark();
-
-	GLuint vertex_buffer, transform_buffer, index_buffer, vertex_array, vertex_shader, fragment_shader, program;
-
-	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-	glCompileShader(vertex_shader);
-
-	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-	glCompileShader(fragment_shader);
-
-	program = glCreateProgram();
-	glAttachShader(program, vertex_shader);
-	glAttachShader(program, fragment_shader);
-	glLinkProgram(program);
-
-	GLint v_location, p_location, vpos_location, vcol_location, model_location;
-
-	v_location = glGetUniformLocation(program, "ViewMatrix");
-	p_location = glGetUniformLocation(program, "ProjMatrix");
-	model_location = glGetAttribLocation(program, "ModelMatrix");
-	vpos_location = glGetAttribLocation(program, "vPos");
-	vcol_location = glGetAttribLocation(program, "vCol");
-
-	glGenVertexArrays(1, &vertex_array);
-	glBindVertexArray(vertex_array);
-
-	glGenBuffers(1, &vertex_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(vpos_location);
-	glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-		sizeof(vertices[0]), (void*)0);
-
-	glEnableVertexAttribArray(vcol_location);
-	glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-		sizeof(vertices[0]), (void*)(sizeof(float) * 2));
-
-	glGenBuffers(1, &transform_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, transform_buffer);
-	glBufferData(GL_ARRAY_BUFFER, COUNT_TRIANGLES * sizeof(mat4x4), triangleTransforms, GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(model_location);
-	glVertexAttribPointer(model_location, 4, GL_FLOAT, GL_FALSE,
-		sizeof(mat4x4), (void*)0);
-
-	glEnableVertexAttribArray(model_location + 1);
-	glVertexAttribPointer(model_location + 1, 4, GL_FLOAT, GL_FALSE,
-		sizeof(mat4x4), (void*)(1 * sizeof(vec4)));
-
-	glEnableVertexAttribArray(model_location + 2);
-	glVertexAttribPointer(model_location + 2, 4, GL_FLOAT, GL_FALSE,
-		sizeof(mat4x4), (void*)(2 * sizeof(vec4)));
-
-	glEnableVertexAttribArray(model_location + 3);
-	glVertexAttribPointer(model_location + 3, 4, GL_FLOAT, GL_FALSE,
-		sizeof(mat4x4), (void*)(3 * sizeof(vec4)));
-
-	glVertexAttribDivisor(model_location, 1);
-	glVertexAttribDivisor(model_location + 1, 1);
-	glVertexAttribDivisor(model_location + 2, 1);
-	glVertexAttribDivisor(model_location + 3, 1);
-
-	glGenBuffers(1, &index_buffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	unique_ptr<b2World> world = make_unique<b2World>(b2Vec2(0, -9.81f));
+	const auto world = make_unique<b2World>(b2Vec2(0, -9.81f));
 
 	create_world_bounds(world);
 
 	create_physics_triangles(world);
+
+	const auto rate = glfwGetVideoMode(glfwGetPrimaryMonitor())->refreshRate;
 
 	float gravityModifier = 0;
 	float clearColor[3] = { 0.2f, 0.2f, 0.2f };
@@ -249,25 +293,24 @@ int main()
 
 		world->SetGravity({ 0, -9.81f * gravityModifier });
 
-		float ratio;
 		int width, height;
 		mat4x4 m, v, p;
 
 		glfwGetFramebufferSize(window, &width, &height);
-		ratio = width / (float)height;
+		const float ratio = static_cast<float>(width) / static_cast<float>(height);
 
 		glViewport(0, 0, width, height);
 		glClearColor(clearColor[0], clearColor[1], clearColor[2], 1);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		world->Step(1 / 60.0f, 6, 2);
+		world->Step(1.0f / static_cast<float>(rate), 20, 10);
 
 		// Set camera
 		mat4x4_identity(v);
 		mat4x4_translate(v, 0, 0, 0);
 		mat4x4_invert(v, v);
 
-		float zoom = 1;
+		constexpr float zoom = 7;
 
 		// Projection
 		mat4x4_ortho(p, -ratio * zoom, ratio* zoom, -zoom, zoom, 1.0f, -1.0f);
@@ -288,8 +331,8 @@ int main()
 
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
 
-		glUniformMatrix4fv(v_location, 1, GL_FALSE, (const GLfloat*)v);
-		glUniformMatrix4fv(p_location, 1, GL_FALSE, (const GLfloat*)p);
+		glUniformMatrix4fv(v_location, 1, GL_FALSE, reinterpret_cast<const GLfloat*>(v));
+		glUniformMatrix4fv(p_location, 1, GL_FALSE, reinterpret_cast<const GLfloat*>(p));
 
 		glDrawElementsInstanced(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr, COUNT_TRIANGLES);
 
--- NetworkingPhysics.h
+++ NetworkingPhysics.h
@@ -2,9 +2,10 @@
 
 #define GLFW_INCLUDE_NONE
 
-#include <iostream>
 #include <box2d/box2d.h>
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include <linmath.h>
 #include <imgui/imgui.h>
+#include <asio.hpp>
+#include <iostream>
